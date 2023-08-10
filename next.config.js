9789fc95cb8fd91faceb877d996edb6ea8150064
Matthew32 <mmartinezmateo93@gmail.com>
2023-08-10 22:40:30 +0200

    Added next directives

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+        assetPrefix: '/mhfz',
+        rewrites() {
+          return [
+            { source: '/mhfz/_next/:path*', destination: '/_next/:path*' }
+          ]
+        }
+}
 
 module.exports = nextConfig
